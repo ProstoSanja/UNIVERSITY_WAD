6dd77525b70b631d1569cded9953a31939854617
Alex Tsernoh <foralwork@gmail.com>
2020-02-10 22:16:28 +0000

    ch 10.2

--- rango/views.py
+++ rango/views.py
@@ -24,7 +24,6 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = pages_list
-    context_dict['visits'] = request.session['visits']
     # Render the response and send it back!
     response = render(request, 'rango/index.html', context=context_dict)
     return response
@@ -33,7 +32,12 @@ def index(request):
 def about(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'This tutorial has been put together by Alex Tsernoh!'}
+
+
+    visitor_cookie_handler(request)
+
+    context_dict = {'boldmessage': 'This tutorial has been put together by Alex Tsernoh!',
+                    'visits': request.session['visits']}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
